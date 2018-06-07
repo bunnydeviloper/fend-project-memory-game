c9f22682fb28043b3c03b003bee09f0aa4ae965b
Sophia <phuongmai001@gmail.com>
2018-06-06 17:41:06 -0700

    minor refactoring 2

--- js/app.js
+++ js/app.js
@@ -2,11 +2,15 @@
 const cards = document.getElementsByClassName('card');
 const initialDeck = [...cards];
 const deck = document.querySelector('.deck');
+const displayMoves = document.querySelector('.moves');
+const restart = document.querySelector('.restart');
+
+let moves = 0; // set initial move to 0
+let openCards = []; // temporary list of open cards
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
-
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
@@ -14,13 +18,13 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
-
     return array;
 }
 
 const shuffleDeck = function() {
-  // shuffle the list of cards using the provided "shuffle" method 
+  // shuffle the list of cards using the provided "shuffle" method above
   shuffle(initialDeck);
+  
   // loop through each card and create its HTML
   for (let i = 0; i < initialDeck.length; i++) {
     const newCard = initialDeck[i];
@@ -30,25 +34,20 @@ const shuffleDeck = function() {
 }
 
 const startGame = function() {
+  moves = 0;
+  displayMoves.innerText = moves;
   // reset all the cards properties
   initialDeck.forEach(function(card) {
     card.classList.remove('show', 'open', 'match');
   });
-  // hide all the initial cards
-  openCards = [];
+  openCards = []; // hide all the initial cards
   shuffleDeck();
-
 };
 
 // Display the cards on the page
 window.onload = function() { startGame(); }; 
-
-/*
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-
-let openCards = []; // temporary list of open cards
+// Restart when user clicked on the restart button
+restart.addEventListener('click', startGame);
 
 const matchOrNot = function(status1, status2) {
   openCards.forEach((e) => { 
@@ -58,30 +57,29 @@ const matchOrNot = function(status1, status2) {
   openCards = []; // reset list of open cards to none
 };
 
-// Add open cards to array
-const addOpenCards = function() {
+const addCards = function() {
+  // increment the move counter and display it on the page (fn)
+  moves++;
+  displayMoves.innerText = moves;
+
+  // each click, flip the card and display the card's symbol
+  this.classList.toggle('open');
+  this.classList.toggle('show');
+
+  // Add open cards to array
   openCards.push(this);
+
   if (openCards.length === 2) {
     // check if 2 current cards are match
     (openCards[0].querySelector('i').classList.value ===
      openCards[1].querySelector('i').classList.value)
       ? matchOrNot('match') 
       : setTimeout(function() { matchOrNot('open', 'show') }, 500); // delay half second so user can see cards
+    return; // soft reset after user have seen 2 cards
   }
 };
 
-// Each click, flip the card and display the card's symbol
-const flipCard = function() {
-  if (openCards.length === 2) return; // only allow user to see maximum 2 cards at a time
-  this.classList.toggle('open');
-  this.classList.toggle('show');
-};
-
 // Set up click event listener for each card
-initialDeck.forEach(function(card) {
-  card.addEventListener('click', flipCard);
-  card.addEventListener('click', addOpenCards);
-});
+initialDeck.forEach(function(card) { card.addEventListener('click', addCards); });
 
-const restart = document.querySelector('.restart');
-restart.addEventListener('click', startGame);
+// if all cards have matched, display a message with the final score (fn)
